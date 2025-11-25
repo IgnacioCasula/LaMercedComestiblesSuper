8ec62d7540c96675c361027b0c65ef7eb37d4b00
ignacio casula <nachocasula@gmail.com>
2025-11-25 11:21:37 -0300

    cambios de aaron

--- ventas/urls.py
+++ ventas/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('registrar/', views.registrar_venta, name='registrar_venta'),
     path('buscar-producto/', views.buscar_producto, name='buscar_producto'),
     path('procesar-venta/', views.procesar_venta, name='procesar_venta'),
+    # ✅ NUEVA URL PARA ACTUALIZAR STOCK
+    path('obtener-stock-actualizado/', views.obtener_stock_actualizado, name='obtener_stock_actualizado'),
 ]
